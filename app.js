26ace58783ba13fadf2b1298df85924f0e52ddfe
Mohammed Rakib <mohammedrakib81336@gmail.com>
2021-02-11 11:35:32 +0600

    fainal commit

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 
 const secrchBtn = document.getElementById('search-btn');
 
-secrchBtn.addEventListener("click",function(){
+
+// secrchBtn.addEventListener("click", async function(){
+//     const searchSong = document.getElementById('search-fild').value;
+//     const url = `https://api.lyrics.ovh/suggest/${searchSong}`;
+//     const res =await fetch(url);
+//     const data =await res.json();
+//     displaySongs(data.data);
+
+    
+// })
+secrchBtn.addEventListener("click", function(){
     const searchSong = document.getElementById('search-fild').value;
     const url = `https://api.lyrics.ovh/suggest/${searchSong}`;
-    fetch(url)
-    .then( res => res.json())
+     fetch(url)
+    .then(res=> res.json())
     .then(data => displaySongs(data.data))
+    .catch( error => alert(error));
 
-    .catch(err => alert("Sorry this song is not available!"))
+    
 })
 
 displaySongs = songs => {
     const displaySong = document.getElementById('display');
+    displaySong.innerText= '';
     displaySong.innerText='';
     songs.forEach(song => {
-        console.log(song);
         const songDiv = document.createElement('div');
         songDiv.className=("single-result row align-items-center my-3 p-3");
         const artistName = song.artist;
@@ -25,7 +36,7 @@ displaySongs = songs => {
         <p class="author lead">Album by <span>${artistName.name}</span></p>
         </div>
         <div class="col-md-3 text-md-right text-center">
-            <button class="btn btn-success">Get Lyrics</button>
+            <button onclick="getLyric('${artistName.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
         </div>
         <audio class="m-5 " controls="controls">
         <source src="${song.preview}" type="audio/ogg">
@@ -37,22 +48,28 @@ displaySongs = songs => {
     });
 }
 
-// displaySongs = songs => {
-//     const dispalySong = document.getElementById('display');
-    
-//     for (let i = 0; i < songs.length; i++) {
-//         const element = songs[i];
-//         const pictures = element.album;
-//         // console.log(pictures);
-//         // const display = element.link;
-//         const div = document.createElement("div");
-//         div.className="col-md-4";
-//         const coloum1 = `
-//         <img src="${pictures.cover_medium}">
-//         <h2>${element.title}</h2>
-//      `
-//      div.innerHTML=coloum1;
-//      dispalySong.appendChild(div);
+const getLyric = async(artist,title) => {
+    const url = (`https://api.lyrics.ovh/v1/${artist}/${title}`);
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLyrics(data.lyrics)
+    }
+    catch (error){
+        console.log(error)
+    }
 
-//     }
-// }
+}
+const displayLyrics = lyrics =>{
+    const lyricsDiv =document.getElementById('songLyrics');
+    lyricsDiv.innerText='';
+    lyricsDiv.innerText = lyrics;
+}
+
+
+    // const getLyric = (artist,title) => {
+    //     const url = (`https://api.lyrics.ovh/v1/${artist}/${title}`);
+    //     fetch(url)
+    //     .then(res => res.json())
+    //     .then(data =>  displayLyrics(data.lyrics))
+    // }
